474206bf46936a9aaae94d5e51b32332056803e2
er.rokade@gmail.com <er.rokade@gmail.com>
2025-04-06 11:07:00 +0530

    [Ajay]star program add new

--- src/test/java/logicalPrograms/Eg9.java
+++ src/test/java/logicalPrograms/Eg9.java
@@ -8,14 +8,16 @@ public class Eg9
 	 
 	 int row=5;
 	 int colum=5;
+	 int star=5;
 	 
 	 for(int i=1;i<=row;i++)
 	 {
-		 for(int j=1;j<=colum;j++)
+		 for(int j=1;j<=star;j++)
 		 {
 			 System.out.print("*");
 		 }
 		 System.out.println();
+		 star--;
 		
 	 }
 	 

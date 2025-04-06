aeb1e06311a35910bddfbc98963d740b45f208ed
er.rokade@gmail.com <er.rokade@gmail.com>
2025-04-06 10:14:01 +0530

    [Ajay]pattern program upload on branch

--- src/test/java/logicalPrograms/Eg9.java
+++ src/test/java/logicalPrograms/Eg9.java
@@ -3,67 +3,22 @@ public class Eg9
 {
  public static void main(String[] args)
  {
-	//1.find no is prime or not
+
+	 //star pattern program
 	 
-//	 int number=13;
-//	 int count=0;
-//	 
-//	 for(int i=2;i<=number;i++)
-//	 {
-//		  if(number%i==0)
-//		  {
-//			  count++;
-//			  break;
-//			
-//		  }	 
-//	 }
-//	 
-//	 if(count==0)
-//	 {
-//		 System.out.println("its prime number ");
-//	 }
-//	 else
-//	 {
-//		 System.out.println("its not a prime number ");
-//	 }
+	 int row=5;
+	 int colum=5;
 	 
-	 
-	 //2.count and print vowels
-	 //3.remove vowels
-	 //4.find duplicate elements in array
-	//5.find duplicate string elements in array
-	 //6.find out the lowest elementin array
-	 //7.reverse integer no by using string methods
-	 
-	//8.reverse numbre
-	 
-//	 int a=123456;
-//	 int rev=0;
-//	 
-//	 while(a>0)
-//	 {
-//		 rev=rev*10+a%10;
-//		 a=a/10;
-//	 }
-//	 System.out.println(rev);
-		
-	 //8.array number revese
-	 int a1[]= {1,2,3,4,5};
-	 
-	 int rev1=0;
-	 
-	 
-	 for(int i=1;i<=a1.length;i++)
+	 for(int i=1;i<=row;i++)
 	 {
-		 System.out.println(i);
+		 for(int j=1;j<=colum;j++)
+		 {
+			 System.out.print("*");
+		 }
+		 System.out.println();
+		
 	 }
 	 
-//	
-//	 for(int i=a1.length;i>0;i--)
-//	 {
-//		 System.out.println(i); 
-//	 }
-	 
-	 System.out.println("hi its Eg9 i am updated this class");
+	// System.out.println("hi its Eg9 i am updated this class");
 	 }
 }
